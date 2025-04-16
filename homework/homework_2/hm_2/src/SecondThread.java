b4232e67d4d1e3f617de1fb8a3226dd5d39fecec
EvgeniiBuller <buller.1@gmx.de>
2025-04-16 19:35:18 +0200

    Homework_2 commit

---
+++ homework/homework_2/hm_2/src/SecondThread.java
@@ -0,0 +1,19 @@
+public class SecondThread extends Thread {
+
+
+    @Override
+    public void run() {
+        for (int i = 1; ; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Поток делится на 3: " + i);
+            }
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                System.out.println("Поток делится на 3 прерван.");
+                return;
+            }
+        }
+    }
+}
+
