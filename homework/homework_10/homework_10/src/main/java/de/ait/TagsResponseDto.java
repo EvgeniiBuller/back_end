79f0b32e9006498209818866320e5856a3f65004
EvgeniiBuller <buller.1@gmx.de>
2025-05-03 15:03:10 +0200

    homework_10 commit

---
+++ homework/homework_10/homework_10/src/main/java/de/ait/TagsResponseDto.java
@@ -0,0 +1,13 @@
+package de.ait;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+@Getter
+
+public class TagsResponseDto {
+    private ResultDto result;
+}
