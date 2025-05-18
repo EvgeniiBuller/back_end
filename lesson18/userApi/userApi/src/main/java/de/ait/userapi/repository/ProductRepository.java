5602418d9b656afc71996b8bc8058a488067a7cb
EvgeniiBuller <buller.1@gmx.de>
2025-05-18 12:26:09 +0200

    lesson18

---
+++ lesson18/userApi/userApi/src/main/java/de/ait/userapi/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package de.ait.userapi.repository;
+
+import de.ait.userapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findAll();
+    Optional<Product> findById(Long id);
+    Product save(Product product);
+
+    //
+    List<Product> findByTitleStartsWithOrderById(String titlePrefix);
+    Long countAllByTitle(String title);
+    void deleteByTitle(String title);
+}
+
